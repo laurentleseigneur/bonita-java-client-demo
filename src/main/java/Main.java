a475f07a696f967fa4ab8719bbd9a67dca784a06
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2020-11-10 17:32:12 +0100

    switch to new bonita java client lib (#2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,66 +1,97 @@
-import com.bonitasoft.engine.api.APIClient;
-import com.bonitasoft.engine.api.ProcessAPI;
-import org.bonitasoft.engine.api.ApiAccessType;
-import org.bonitasoft.engine.api.ApplicationAPI;
-import org.bonitasoft.engine.bpm.bar.BusinessArchive;
-import org.bonitasoft.engine.bpm.bar.BusinessArchiveFactory;
-import org.bonitasoft.engine.bpm.bar.InvalidBusinessArchiveFormatException;
-import org.bonitasoft.engine.bpm.process.ProcessDefinition;
-import org.bonitasoft.engine.bpm.process.ProcessDeployException;
-import org.bonitasoft.engine.bpm.process.ProcessDeploymentInfo;
-import org.bonitasoft.engine.bpm.process.ProcessEnablementException;
-import org.bonitasoft.engine.business.application.Application;
-import org.bonitasoft.engine.exception.AlreadyExistsException;
-import org.bonitasoft.engine.exception.SearchException;
-import org.bonitasoft.engine.platform.LoginException;
-import org.bonitasoft.engine.search.SearchOptionsBuilder;
-import org.bonitasoft.engine.search.SearchResult;
-import org.bonitasoft.engine.util.APITypeManager;
+import org.bonitasoft.web.client.BonitaClient;
+import org.bonitasoft.web.client.model.ProcessDefinition;
+import org.bonitasoft.web.client.model.Session;
+import org.bonitasoft.web.client.services.policies.ApplicationImportPolicy;
+import org.bonitasoft.web.client.services.policies.OrganizationImportPolicy;
+import org.bonitasoft.web.client.services.policies.ProcessImportPolicy;
+import org.bonitasoft.web.client.services.policies.ProfileImportPolicy;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		String bonitaUrl = args[0];
-		String pathToJarFile = args[1];
+		String username = args[1];
+		String password = args[2];
 
-		System.out.println("bonitaUrl:" + bonitaUrl);
-		System.out.println("pathToJarFile:" + pathToJarFile);
+		File organizationFile = getFileFromArgs(args, "organization");
+		File profilesFile = getFileFromArgs(args, "profiles");
+		File bdmFile = getFileFromArgs(args, "bdmFile");
+		File accessControlFile = getFileFromArgs(args, "accessControl");
+		File pageZipFile = getFileFromArgs(args, "pageZip");
+		File applicationFile = getFileFromArgs(args, "application");
+		File processFile = getFileFromArgs(args, "process");
 
-		// Let's set the connection settings to use HTTP on the already running Bonita runtime:
-		Map<String, String> settings = new HashMap<String, String>();
-		settings.put("server.url", bonitaUrl);
-		settings.put("application.name", "bonita");
-		APITypeManager.setAPITypeAndParams(ApiAccessType.HTTP, settings);
+		OrganizationImportPolicy organizationImportPolicy = getOrganizationImportPolicy();
+		ProfileImportPolicy profileImportPolicy = getProfileImportPolicy();
+		ApplicationImportPolicy applicationImportPolicy = getApplicationImportPolicy();
+		ProcessImportPolicy processImportPolicy = getProcessImportPolicy();
 
-		final APIClient apiClient = new APIClient();
-		apiClient.login("walter.bates", "bpm");
-		final ProcessAPI processAPI = apiClient.getProcessAPI();
+		// Create a client
+		BonitaClient client = BonitaClient.builder(bonitaUrl).build();
 
-		//display deployed processes
-		SearchResult<ProcessDeploymentInfo> processDeploymentInfoSearchResult = processAPI.searchProcessDeploymentInfos(new SearchOptionsBuilder(0, 10).done());
-		for (ProcessDeploymentInfo processDeploymentInfo : processDeploymentInfoSearchResult.getResult()) {
-			System.out.println("found process:" + processDeploymentInfo.getName() + " in version:" + processDeploymentInfo.getVersion());
-		}
+		// Login
+		Session session = client.login(username, password);
 
-		//display deployed living application
-		ApplicationAPI applicationAPI = apiClient.getApplicationAPI();
-		SearchResult<Application> applicationSearchResult = applicationAPI.searchApplications(new SearchOptionsBuilder(0, 10).done());
-		for (Application application : applicationSearchResult.getResult()) {
-			System.out.println("found deployed application:" + application.getDisplayName() + " with token:" + application.getToken());
-		}
+		// Log Bonita server version
+		log("Bonita version:" + session.getVersion());
 
-		//ensure this process name/version is not yet deployed on target platform
-		final BusinessArchive businessArchive = BusinessArchiveFactory.readBusinessArchive(new File(pathToJarFile));
-		final ProcessDefinition processDefinition = processAPI.deployAndEnableProcess(businessArchive);
-		System.out.println("deployed new version with id:"+ processDefinition.getId());
-		System.out.println("deployed new version with name:"+ processDefinition.getName());
-		System.out.println("deployed new version with version:"+ processDefinition.getVersion());
+		//Organization
+		client.users().importOrganization(organizationFile, organizationImportPolicy);
 
+		//Profiles
+		client.users().importProfiles(profilesFile, profileImportPolicy);
 
+		//BDM
+		client.bdm().importBDM(bdmFile);
+
+		// BdmAccessControl
+		client.bdm().importBdmAccessControl(accessControlFile);
+
+		// RestApiExtensions
+		// Pages
+		// Layouts
+		// Themes
+		client.applications().importPage(pageZipFile);
+
+		// Applications
+		client.applications().importApplications(applicationFile, applicationImportPolicy);
+
+		// Processes
+		client.processes().importProcess(processFile, processImportPolicy);
+
+		// checks
+		List<ProcessDefinition> processDefinitions = client.processes().searchProcesses(0, 20);
+
+		// Logout when done
+		client.logout();
+
+	}
+
+	private static File getFileFromArgs(String[] args, String argName) {
+		return new File("some file from args");
+	}
+
+	private static ProcessImportPolicy getProcessImportPolicy() {
+		return ProcessImportPolicy.FAIL_ON_DUPLICATES;
+	}
+
+	private static ProfileImportPolicy getProfileImportPolicy() {
+		return ProfileImportPolicy.IGNORE_IF_ANY_EXISTS;
+	}
+
+	private static ApplicationImportPolicy getApplicationImportPolicy() {
+		return ApplicationImportPolicy.FAIL_ON_DUPLICATES;
+	}
+
+	private static OrganizationImportPolicy getOrganizationImportPolicy() {
+		return OrganizationImportPolicy.FAIL_ON_DUPLICATES;
+	}
+
+	private static void log(String message) {
+		System.out.println(message);
 	}
 
 }
